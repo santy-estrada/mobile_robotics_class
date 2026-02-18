cd7079f45f39eb4abc18670d58e980bf6319f28e
Santiago Estrada Bernal <santiago.estrada6@eia.edu.co>
2026-02-18 13:34:55 -0500

    Update priority settings in twist_mux.yaml and clean up control_gap_ttc.py

--- delta_follow_the_gap/delta_follow_the_gap/control_gap_ttc.py
+++ delta_follow_the_gap/delta_follow_the_gap/control_gap_ttc.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist, TwistStamped
-import math
 from std_msgs.msg import Bool
 
 
@@ -11,28 +10,12 @@ class ControlNode(Node):
         super().__init__('control_gap_ttc')
 
         # -- PARAMETERS --
-        self.declare_parameter('kp', 0.5)                    # Proportional gain for PD controller
-        self.declare_parameter('kd', 0.9)                    # Derivative gain for PD controller
-        self.declare_parameter('max_steering', math.radians(60))          # Max steering angle saturation (radians)
-        self.declare_parameter('min_steering', math.radians(-60))         # Min steering angle saturation (radians)
         self.declare_parameter('forward_velocity', 2.0)      # Constant forward velocity (m/s)
-        self.declare_parameter('brake_turn_angle', 1.3) #Puede ser 1.0 si fv = 2.0
-        self.declare_parameter('start_flag', False)               #Flag to signal that the car has received its first forward input from Joystickz
-        self.declare_parameter('pub_logger', True)               #Flag to signal whether to publish error values for logging in ttc_gap_logger_node
-        
-
+        self.declare_parameter('start_flag', False)          # Flag to signal that the car has received its first forward input from Joystick
+        self.declare_parameter('pub_logger', True)           # Flag to signal whether to publish error values for logging
 
         # Get parameters
-        self.kp = self.get_parameter('kp').value
-        self.kd = self.get_parameter('kd').value
         self.forward_vel = self.get_parameter('forward_velocity').value
-        self.max_steering = float(self.get_parameter('max_steering').value)
-        self.min_steering = float(self.get_parameter('min_steering').value)
-        self.brake_turn_angle= float(self.get_parameter('brake_turn_angle').value)
-        # State variables for derivative calculation
-        self.prev_error = 0.0
-        self.prev_time = None
-        # Brake state
         self.start_flag = bool(self.get_parameter('start_flag').value)
         self.pub_logger = bool(self.get_parameter('pub_logger').value)
 
@@ -65,53 +48,40 @@ class ControlNode(Node):
         if msg.data:
             self.get_logger().info("Received start signal. Starting control.")
             self.start_flag = True
+            cmd = TwistStamped()
+            cmd.twist.linear.x = self.forward_vel
+            cmd.twist.angular.z = -5.14  # Initial turn to start following the gap
+            self.cmd_pub.publish(cmd)
 
     def error_callback(self, msg: Twist):
         """
         Callback for error messages from dist_finder.
         
-        msg.linear.x: distance error from wall (y)
-        msg.linear.y: distance travelled since last callback (L or AC)
+        msg.angular.y: alert flag
         msg.angular.z: angular error / orientation angle (theta or alpha)
         """
-
         if not self.start_flag:
             self.get_logger().info("Controller not started yet. Ignoring error messages.")
             return
-    
-        current_time = self.get_clock().now()
-
-        if self.prev_time is None:
-            self.prev_time = current_time
-            self.prev_error = 0.0
-            return
-
-        # Calculate time step
-        dt = (current_time - self.prev_time).nanoseconds * 1e-9
-        self.prev_time = current_time
 
         # Extract error components
-        y = msg.linear.x           # Distance error from wall
-        L = msg.linear.y           # Distance travelled (AC)
-        theta =msg.angular.z     # Angular error (alpha)
-        alertado=msg.angular.y 
+        theta = msg.angular.z      # Angular error (alpha)
+        alertado = msg.angular.y   # Alert flag
+        
         # Create and publish command
         cmd = TwistStamped()
         if alertado == 1.0:
-            cmd.twist.linear.x = 0.8*self.forward_vel
-            cmd.twist.angular.z = 1.7*theta  
+            cmd.twist.linear.x = 0.8 * self.forward_vel
+            cmd.twist.angular.z = 1.7 * theta
         else:
-            cmd.twist.linear.x = 1.3*self.forward_vel
-            cmd.twist.angular.z = 1.7*theta  
-
+            cmd.twist.linear.x = 1.3 * self.forward_vel
+            cmd.twist.angular.z = 1.7 * theta
 
         self.cmd_pub.publish(cmd)
 
         # Logging
         if self.pub_logger:
-            self.get_logger().info(
-                f"crr_env={theta} | "
-            )
+            self.get_logger().info(f"crr_env={theta} | ")
 
 
 def main(args=None):
